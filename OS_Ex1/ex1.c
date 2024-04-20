bc0f6474bcae81f193f97c6a52325aa800090655
avi-lezerov <avrahamle16@gmail.com>
2024-04-20 16:01:03 +0300

    Refactor file opening and error handling in copy_file function in ex1.c

--- OS_Ex1/ex1.c
+++ OS_Ex1/ex1.c
@@ -23,57 +23,76 @@ void exit_with_usage(const char *message) {
 	exit(EXIT_FAILURE);
 }
 
+/**
+ * an helper function to open the files
+ * @param source_file the name of the source file
+ * @param dest_file the name of the destination file
+ * @param force_flag a flag to indicate if the destination file should be overwritten
+ * @param source_fd a pointer to the file descriptor of the source file
+ * @param dest_fd a pointer to the file descriptor of the destination file
+*/
+void open_files(const char *source_file, const char *dest_file, int force_flag, int *source_fd, int *dest_fd){
+    *source_fd = open(source_file, O_RDONLY);
+    if (*source_fd == -1) {
+        perror("Unable to open source file for reading");
+        exit(EXIT_FAILURE);
+    }
+
+    int flags = O_WRONLY | O_CREAT;
+    if (force_flag) {
+        flags |= O_TRUNC;
+    } else {
+        flags |= O_EXCL;
+    }
+
+    *dest_fd = open(dest_file, flags, DESTINATION_FILE_MODE);
+    if (*dest_fd == -1) {
+        perror("Unable to open destination file for writing");
+        close(*source_fd);
+        exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * an helper function to close the files
+ * @param source_fd the file descriptor of the source file
+*/
+void close_files(int source_fd, int dest_fd){
+	if (close(source_fd) == -1) {
+			perror("Unable to close source file");
+			close(dest_fd);
+			exit(EXIT_FAILURE);
+	}
+	if (close(dest_fd) == -1) {
+		perror("Unable to close destination file");
+		exit(EXIT_FAILURE);
+	}
+}
+	
+
 void copy_file(const char *source_file, const char *dest_file, int buffer_size, int force_flag) {
 	int source_fd, dest_fd;
     ssize_t bytes_read, bytes_written;
 	char buffer[buffer_size];
 
-	source_fd = open(source_file, O_RDONLY);
-	if (source_fd == -1) {
-		perror("Unable to open source file for reading");
-		exit(EXIT_FAILURE);
-	}
-
-	int flags = O_WRONLY | O_CREAT ;
-	if (force_flag) {
-		flags |= O_TRUNC;
-
-	}else {
-		flags |= O_EXCL;
-	}
-
-	dest_fd = open(dest_file, flags, DESTINATION_FILE_MODE);
-	if (dest_fd == -1) {
-		perror("Unable to open destination file for writing");
-		close(source_fd);
-		exit(EXIT_FAILURE);
-	}
+	open_files(source_file, dest_file, force_flag, &source_fd, &dest_fd);
 	
 	while((bytes_read = read(source_fd, buffer, buffer_size)) > 0) {
 		bytes_written = write(dest_fd, buffer, bytes_read);
 		if (bytes_written == -1) {
 			perror("Unable to write to destination file");
-			close(source_fd);
-			close(dest_fd);
+			close_files(source_fd, dest_fd);
 			exit(EXIT_FAILURE);
-		}
+			}
 		if (bytes_written != bytes_read) {
             fprintf(stderr, "Unable to write buffer content to destination file\n");
-            close(source_fd);
-            close(dest_fd);
+            close_files(source_fd, dest_fd);
             exit(EXIT_FAILURE);
-        }
-	}
+        	}
+		}
+	
+		close_files(source_fd, dest_fd);
 
-		if (close(source_fd) == -1) {
-			 perror("Unable to close source file");
-			close(dest_fd);
-			exit(EXIT_FAILURE);
-		}
-		if (close(dest_fd) == -1) {
-			perror("Unable to close destination file");
-			exit(EXIT_FAILURE);
-		}
 		printf("File %s was successfully copied to %s\n",source_file, dest_file);
 	}
 	
